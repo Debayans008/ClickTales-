a9a480c728e682ba322b2e73d0d88b7e0c38e7a8
Debayan Saha <debayans008@gmail.com>
2025-07-17 14:50:14 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Camera from './components/Camera';
+import './style.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>📸 ClickTales</h1>
+      <Camera />
+    </div>
+  );
+}
+
+export default App;
